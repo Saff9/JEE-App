806ad8980c0895039f2de6c411aa2ee8a9831e12
Saff9 <saffanakbar942@gmail.com>
2025-10-27 17:17:05 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['i.ytimg.com'],
+  },
+}
+
+module.exports = nextConfig

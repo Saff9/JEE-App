aa363b5feafa2565a7c589f4e23b800fa0077950
Saff9 <saffanakbar942@gmail.com>
2025-10-28 12:27:08 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,11 @@
-// app/layout.js
-import './globals.css';
-
 export const metadata = {
   title: 'JEE Prep Pro',
-  description: 'JEE Study App',
-};
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  );
+  )
 }
